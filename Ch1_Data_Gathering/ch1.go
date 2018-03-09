eb3c685ddfa8875cd7ca0f57ae60e08f2c5d1441
jrakhman <juniar@rithm.nl>
2018-03-09 11:37:34 +0700

    commit 2

--- Ch1_Data_Gathering/ch1.go
+++ Ch1_Data_Gathering/ch1.go
@@ -3,11 +3,9 @@ package main
 import (
 	"encoding/csv"
 	"fmt"
-	"io"
 	"log"
 	"os"
 	"path"
-	"strconv"
 
 	"github.com/kniren/gota/dataframe"
 )
@@ -32,63 +30,66 @@ func main() {
 	reader := csv.NewReader(file)
 	reader.FieldsPerRecord = 0
 
-	var csvRecord CSVRecord
-	var csvData []CSVRecord
+	//var csvRecord CSVRecord
+	//var csvData []CSVRecord
+	//
+	//for {
+	//	record, err := reader.Read()
+	//	if err == io.EOF {
+	//		log.Println("reached EOF")
+	//		break
+	//	}
+	//
+	//	for i, value := range record {
+	//
+	//		var floatVal float64
+	//
+	//		if i == 4 {
+	//			if value == "" {
+	//				log.Printf("Unexpected type in column %d\n", i)
+	//				csvRecord.ParseError = fmt.Errorf("Empty string value")
+	//				break
+	//			}
+	//
+	//			csvRecord.Species = value
+	//			continue
+	//		}
+	//
+	//		if floatVal, err = strconv.ParseFloat(value, 64); err != nil {
+	//			log.Printf("Unexpected type in column %d\n", i)
+	//			csvRecord.ParseError = fmt.Errorf("could not parse float : %s", value)
+	//			break
+	//		}
+	//
+	//		switch i {
+	//		case 0:
+	//			csvRecord.SepalLength = floatVal
+	//		case 1:
+	//			csvRecord.SepalWidth = floatVal
+	//		case 2:
+	//			csvRecord.PetalLength = floatVal
+	//		case 3:
+	//			csvRecord.PetalWidth = floatVal
+	//		}
+	//	}
+	//
+	//	if csvRecord.ParseError == nil {
+	//		csvData = append(csvData, csvRecord)
+	//	}
+	//}
 
-	for {
-		record, err := reader.Read()
-		if err == io.EOF {
-			log.Println("reached EOF")
-			break
-		}
-
-		for i, value := range record {
-
-			var floatVal float64
-
-			if i == 4 {
-				if value == "" {
-					log.Printf("Unexpected type in column %d\n", i)
-					csvRecord.ParseError = fmt.Errorf("Empty string value")
-					break
-				}
-
-				csvRecord.Species = value
-			}
-
-			if floatVal, err = strconv.ParseFloat(value, 64); err != nil {
-				log.Printf("Unexpected type in column %d\n", i)
-				csvRecord.ParseError = fmt.Errorf("could not parse float : %s", value)
-				break
-			}
-
-			switch i {
-			case 0:
-				csvRecord.SepalLength = floatVal
-			case 1:
-				csvRecord.SepalWidth = floatVal
-			case 2:
-				csvRecord.PetalLength = floatVal
-			case 3:
-				csvRecord.PetalWidth = floatVal
-			}
-		}
-
-		if csvRecord.ParseError == nil {
-			csvData = append(csvData, csvRecord)
-		}
-	}
-
-	csvDF := dataframe.LoadStructs(csvData)
+	//csvDF := dataframe.LoadStructs(csvData)
 	irisDF := dataframe.ReadCSV(file)
 
-	fmt.Println(csvDF)
+	//fmt.Println(csvDF)
 
 	fmt.Println(irisDF)
 
-	//versiColorDF := irisDF.Filter(dataframe.F{
-	//	Colname:    "species",
-	//	Comparator: "==",
-	//	Comparando: "Iris-versicolor",
-	//}).Select()
+	versiColorDF := irisDF.Filter(dataframe.F{
+		Colname:    "species",
+		Comparator: "==",
+		Comparando: "Iris-versicolor",
+	}).Select([]string{"sepal_width", "species"})
+
+	fmt.Println(versiColorDF)
 }
